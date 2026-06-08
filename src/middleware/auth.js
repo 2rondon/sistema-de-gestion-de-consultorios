e8f12ba8c0b715972ccb4f9c21593db74392093f
Anderson Rondon Laya <36106845+2rondon@users.noreply.github.com>
2026-06-08 18:55:24 -0400

    Create auth.js

---
+++ src/middleware/auth.js
@@ -0,0 +1,17 @@
+const isAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/auth/login');
+    }
+};
+
+const isAdmin = (req, res, next) => {
+    if (req.session.user && req.session.user.rol === 'admin') {
+        next();
+    } else {
+        res.status(403).send('Acceso denegado');
+    }
+};
+
+module.exports = { isAuthenticated, isAdmin };
